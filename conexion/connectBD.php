fa9c5e77ef787629ffbb07b116b30e6eec669969
jlumaca <lumacajuanmanuel@gmail.com>
2020-07-21 11:15:10 -0300

    asd

--- conexion/connectBD.php
+++ conexion/connectBD.php
@@ -1,6 +1,6 @@
 <?php
 function conectar($sql){
-$conexion = mysqli_connect("34.71.137.32","Juan","lumaca") or die ("Problema con la conexion");
+$conexion = mysqli_connect("34.71.137.32","root","root") or die ("Problema con la conexion");
 mysqli_select_db($conexion,"MuseosBD");
 
 $resultado = mysqli_query($conexion,$sql) or die (mysqli_error($conexion));
